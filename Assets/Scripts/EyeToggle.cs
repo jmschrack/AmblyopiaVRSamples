22bfbd478e3844e9034959e10cf6444abb82e29b
Jonathan Schrack <jonathan.schrack@digitalprecept.com>
2017-04-10 12:43:40 -0500

    Initial Project Commit.  Basice Left/Right eye separation for the ship flyer and maze runner. 180 shooting gallery has eye separation, but it isn't playable.

---
+++ Assets/Scripts/EyeToggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeToggle : MonoBehaviour {
+	public Camera cam1;
+	public Camera cam2;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire2")) {
+			StereoTargetEyeMask temp = cam1.stereoTargetEye;
+			cam1.stereoTargetEye = cam2.stereoTargetEye;
+			cam2.stereoTargetEye = temp;
+		}
+	}
+}
